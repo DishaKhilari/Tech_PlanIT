bcc22382f875511300561534766e6547a470051f
DishaOwitechs <132178941+DishaOwitechs@users.noreply.github.com>
2024-02-21 10:46:20 +1100

    File updated Rundriver

--- src/test/java/starter/StartBrowser/RunDriver.java
+++ src/test/java/starter/StartBrowser/RunDriver.java
@@ -5,10 +5,7 @@ public class RunDriver extends BaseClass{
 
     public void setUp(String URL) throws InterruptedException {
         BaseClass.openbrowser("Firefox");
-        if (!options.setHeadless(true).is(String.valueOf(true)))
-        {
-            BaseClass.maximize();
-        }
+//        BaseClass.maximize();
         BaseClass.deletecookies();
 //        driver.manage().timeouts().pageLoadTimeout(Duration.ofDays(40000)); //calculated as milliseconds
         BaseClass.SiteURL(URL);
